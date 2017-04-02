44d5748c8def8d2bcc1d9df058bfd0b319d6ca11
gitlinkberkeley <geoffrey.link@gmail.com>
2017-04-01 21:34:59 -0400

    removed softlayer messaging
    way too slow

--- python/cricket_message_generator.py
+++ python/cricket_message_generator.py
@@ -3,50 +3,71 @@
 import random
 import os
 import argparse
-import softlayer_messaging
 import datetime
+from influxdb import InfluxDBClient
 
-parser = argparse.ArgumentParser(description = 'generate data for softlayer queue')
-parser.add_argument(	'queue_name'	, help = 'queue name'		)
-parser.add_argument(	'host_name'	, help = 'host name'		)
-parser.add_argument(	'data_center'	, help = 'ex: sjc01'		)
-parser.add_argument(	'file_name'	, help = 'ex: roy_data.txt'	)
+parser = argparse.ArgumentParser(description = 'generate data for softlayer queue'		)
+parser.add_argument(    'influx_external_ip'    , help = 'ex: 169.53.133.132'			)
+parser.add_argument(	'device_name'		, help = 'cricket_001_01'			)
+parser.add_argument(	'data_center'		, help = 'ex: sjc01'				)
+parser.add_argument(	'file_name'		, help = 'ex: /root/cricket_001_01_data.txt'	)
 args = parser.parse_args()
 
-# IF roy_data.txt ALREADY EXISTS - DO NOT RUN! - CRICKET GENERATOR IS ALREADY RUNNING #################
+# IF FILE ALREADY EXISTS - DO NOT RUN! - CRICKET GENERATOR IS ALREADY RUNNING FOR THAT DEVICE #################
 
 if not os.path.isfile(args.file_name):
 
-# GENERATE DATA ########################################################################################
+# GENERATE DATA ###############################################################################################
 
   number_of_measurements  = 100
   number_of_measures      =  10
 
   f = open(args.file_name, 'w')
 
-  for i in range(1,number_of_measurements+1):
-    for j in range(1,number_of_measures+1):
-      f.write('measurement'+"%03d" % i+','+str(random.uniform(0, 100))+'\n')
+  for i in range( 1 , number_of_measurements + 1 ):
+
+    for j in range( 1 , number_of_measures + 1 ):
+
+      measurement	=	'measurement' + "%03d" % i
+      value		=	str(random.uniform( 0 , 100 ))
+      device		=	args.device_name + ',' + args.data_center + ',' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+      f.write(device + ',' + measurement + ',' + value + '\n')
 
   f.close()
 
-# PUSH DATA TO SOFTLAYER QUEUE #######################################################################
+# POPULATE INFLUXDB DIRECTLY BECAUSE SOFTLAYER MESSAGING IS TOO SLOW ############################################
 
-  client = softlayer_messaging.get_client('5zdff')
-  client.authenticate('SL1187241', '876a3a7bb1114096ce6a48ab6b246d23e3c9b6717dd115b37063680f92acdd04')
+host            =       args.influx_external_ip
+port            =       8086
+user            =       'cricket'
+password        =       'cricket'
+dbname          =       'cricket_data'
+client_influxdb = InfluxDBClient(host, port, user, password, dbname)
 
-  # all messages are visible in the queue after 10 seconds and expire after 600 seconds
-  # client.create_queue(name='cricket_queue_001', visibility_interval=10, expiration=600)
-  queue = client.queue(args.queue_name)
+f = open(args.file_name, 'r')
 
-  f = open(args.file_name, 'r')
+for line in f:
 
-  for line in f:
-    attribute	=	line.split(',')[0]
-    value	=	line.split(',')[1]
-    device	=	args.host_name +',' + args.data_center +','+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    queue.push(device, fields={attribute : value}, visibility_delay=0)
-    print (device+','+attribute+','+value).strip()
+  device	=	line.strip().split(',')[0]
+  data_center	=	line.strip().split(',')[1]
+  creation_time	=	line.strip().split(',')[2]
+  measurement 	=	line.strip().split(',')[3]
+  value		=	line.strip().split(',')[4]
 
-  f.close()
-  os.remove(args.file_name)
+  json_body = [{"measurement" : measurement,
+                "tags"        : {
+                                 "device"      : device,
+                                 "data_center" : data_center
+                                },
+                "time"         : creation_time,
+                "fields"       : {
+                                  "value" : str(float(value))
+                                 }
+              }]
+
+  print json_body
+  client_influxdb.write_points(json_body)
+
+f.close()
+os.remove(args.file_name)
