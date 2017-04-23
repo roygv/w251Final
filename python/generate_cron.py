341e7f05bbeb0aedd578ffa9c821ec7337a2be24
Thong Bui <thongbui@Thongs-MacBook-Air.local>
2017-04-22 20:08:14 -0700

    Correct the architecture

--- python/generate_cron.py
+++ python/generate_cron.py
@@ -1,4 +1,4 @@
-filename = "cricket.txt"
+filename = "../Docs/crickets.txt"
 print('host', 'IP', 'root_pwd')
 lines = [line.rstrip('\n') for line in open(filename)]
 
@@ -7,7 +7,7 @@ for line in lines:
     cron_file = a[1] + "_cron"
     cfile = open(cron_file, 'w')
     #arr = a[1].split('et')
-    for i in range(1,51):
+    for i in range(1,61):
         cricket = a[1] + "_" + str(i) 
         cfile.write("* * * * * root /root/cricket_message_generator.py 50.23.117.76 " +  cricket + " " +  a[4] + " " + cricket + ".txt > /root/" + cricket + "_message_generator.log 2>&1")
 #    cfile.write("; /root/roy_generator.py > /root/roy_generator.log 2>&1) ")
